b3620b433e298d9f96d7945586dc9935bfada222
miscar-laptop <miscar-laptop@miscar1574.org>
2022-12-05 15:48:15 +0200

    remove: photonlib

--- src/vendordeps/index.ts
+++ src/vendordeps/index.ts
@@ -1,5 +1,4 @@
 import { default as phoenix } from "./phoenix"
 import { default as revlib } from "./revlib"
-import { default as photonlib } from "./photonlib"
 
-export default [phoenix, revlib, photonlib]
+export default [phoenix, revlib]

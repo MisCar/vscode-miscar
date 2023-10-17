555bad717be38b55aeba053efb62d32142fe343f
Peleg Levy <peleg.levy100@gmail.com>
2023-10-17 12:14:06 +0300

    update for 2024

--- src/versions.ts
+++ src/versions.ts
@@ -1,8 +1,8 @@
 export const NI_VERSION = "2023.3.0"
-export const WPILIB_VERSION = "2023.3.2"
+export const WPILIB_VERSION = "2024.1.1-beta-1"
 
-export const OPENCV_VERSION = "4.6.0-3"
-export const FRC_YEAR = "frc2023"
+export const OPENCV_VERSION = "4.8.0"
+export const FRC_YEAR = "frc2024"
 
 export const TOOLCHAIN_VERSION = "v2023-8"
 export const TOOLCHAIN_GCC_VERSION = "12.1.0"

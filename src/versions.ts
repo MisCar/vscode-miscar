5e3ba6d6c38a61d7e0704624ec18cc335141c7c2
miscar-laptop <miscar-laptop@miscar1574.org>
2023-12-31 15:58:09 +0200

    Update NI and OpenCV versions

--- src/versions.ts
+++ src/versions.ts
@@ -1,7 +1,7 @@
-export const NI_VERSION = "2024.1.1"
+export const NI_VERSION = "2024.2.1"
 export const WPILIB_VERSION = "2024.1.1-beta-4"
 
-export const OPENCV_VERSION = "4.8.0-1"
+export const OPENCV_VERSION = "4.8.0-2"
 export const FRC_YEAR = "frc2024"
 
 export const TOOLCHAIN_VERSION = "v2023-8"
